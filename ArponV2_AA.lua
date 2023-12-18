4e5b5afcf80100d7b9bea0fe8602c3086fac3508
Jung Ganmyeon <148054296+junggamyeon@users.noreply.github.com>
2023-12-18 10:25:20 +0700

    Update ArponV2_AA.lua

--- ArponV2_AA.lua
+++ ArponV2_AA.lua
@@ -758,7 +758,7 @@ local dir = "Anime_Adventures/"..game.Players.LocalPlayer.Name
 local Uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArponAG/Scripts/main/finitylibTEST"))()
 --local Uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/ArponAG/Scripts/main/finitylib"))()
 local exec = tostring(identifyexecutor())
-local Window = Uilib.new(true, "[Arpon_V2] Anime Adventures "..version.." - "..exec)
+local Window = Uilib.new(true, "[BETA] Jung Hub "..version.." - "..exec)
 Window.ChangeToggleKey(Enum.KeyCode.P)
 
 local Home = Window:Category("🏠 Home")
